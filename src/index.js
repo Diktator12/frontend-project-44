7c756e75e4b54abf437202f16dd8f9f7081ba146
Diktator12 <tkachev.twilight@yandex.ru>
2025-05-04 19:08:04 +0300

    add a general game logic file

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+import greeting from './cli.js';
+
+const runGame = (description, generateRound) => {
+  console.log('Welcome to the Brain Games!');
+
+  const name = greeting();
+
+  console.log(description);
+
+  const rounds = 3;
+
+  for (let i = 0; i < rounds; i += 1) {
+    const { question, correctAnswer } = generateRound();
+
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default runGame;
