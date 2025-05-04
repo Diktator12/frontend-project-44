4738c350b808e0e600a063075271952bfb382800
Diktator12 <tkachev.twilight@yandex.ru>
2025-05-04 22:09:56 +0300

    update brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,19 +1,4 @@
 #!/usr/bin/env node
-import runGame from '../src/index.js';
+import brainEven from '../src/games/even.js';
 
-const desc = 'Answer "yes" if the number is even, otherwise answer "no"';
-
-const generateRandomNumber = () => Math.floor(Math.random() * 100);
-const isEven = (num) => num % 2 === 0;
-
-const playRound = () => {
-  const number = generateRandomNumber();
-
-  const question = number;
-
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
-
-  return { question, correctAnswer };
-};
-
-runGame(desc, playRound);
+brainEven();
