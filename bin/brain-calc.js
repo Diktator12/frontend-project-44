2e55a5eafd1bcab21f8f64249fe7642750ad8f98
Diktator12 <tkachev.twilight@yandex.ru>
2025-05-04 22:09:12 +0300

    update brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,36 +1,4 @@
 #!/usr/bin/env node
-import runGame from '../src/index.js';
+import brainCalc from '../src/games/calc.js';
 
-const desc = 'What is the result of the expression?';
-
-const generateRandomNumber = () => Math.floor(Math.random() * 100);
-const opIndex = () => Math.floor(Math.random() * 3);
-
-const operations = ['+', '-', '*'];
-
-const playRound = () => {
-  const number1 = generateRandomNumber();
-  const number2 = generateRandomNumber();
-  const roundIndex = opIndex();
-  const selectedOperation = operations[roundIndex];
-
-  const question = `${number1} ${selectedOperation} ${number2}`;
-
-  const calculateAnswer = () => {
-    let result;
-    if (selectedOperation === '+') {
-      result = number1 + number2;
-    } else if (selectedOperation === '-') {
-      result = number1 - number2;
-    } else {
-      result = number1 * number2;
-    }
-    return result.toString();
-  };
-
-  const correctAnswer = calculateAnswer();
-
-  return { question, correctAnswer };
-};
-
-runGame(desc, playRound);
+brainCalc();
