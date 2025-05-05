9eac865a90ef8b2f08dbe3381524f941890a01b3
Diktator12 <tkachev.twilight@yandex.ru>
2025-05-05 12:29:43 +0300

    add prime game

---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+import runGame from '../index.js';
+
+const brainPrime = () => {
+  const desc = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+  const generateRandomNumber = () => Math.floor(Math.random() * 100);
+
+  const playRound = () => {
+    const number = generateRandomNumber();
+
+    const question = `${number}`;
+
+    const calculateAnswer = () => {
+      for (let i = 2; i < number; i++) {
+        if (number % i === 0) return 'no';
+      }
+      return 'yes';
+    };
+
+    const correctAnswer = calculateAnswer();
+
+    return { question, correctAnswer };
+  };
+
+  runGame(desc, playRound);
+};
+
+export default brainPrime;
