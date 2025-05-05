2620a878faf1821750f0dd22654c43fc9aff9b11
Diktator12 <tkachev.twilight@yandex.ru>
2025-05-05 13:21:52 +0300

    fix even game

--- src/games/even.js
+++ src/games/even.js
@@ -3,15 +3,19 @@ import runGame from '../index.js';
 const brainEven = () => {
   const desc = 'Answer "yes" if the number is even, otherwise answer "no"';
 
-  const generateRandomNumber = () => Math.floor(Math.random() * 100);
+  const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1;
   const isEven = (num) => num % 2 === 0;
 
   const playRound = () => {
     const number = generateRandomNumber();
 
-    const question = number;
+    const question = `${number}`;
 
-    const correctAnswer = isEven(number) ? 'yes' : 'no';
+    const calculateAnswer = () => {
+      return isEven(number) ? 'yes' : 'no';
+    };
+
+    const correctAnswer = calculateAnswer();
 
     return { question, correctAnswer };
   };
